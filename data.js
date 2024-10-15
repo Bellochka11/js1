75e51c4263b38b3fc261c61688b56f9a80123952
Bellochka11 <elizavetabelyankina@mail.ru>
2024-10-15 12:19:19 +0300

    g

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_1 = `
+[
+    {
+        "id": 1,
+        "name": "dog_1",
+        "url": "https://images.dog.ceo/breeds/hound-basset/n02088238_9922.jpg"
+    },
+    {
+        "id": 2,
+        "name": "dog_2",
+        "url": "https://images.dog.ceo/breeds/dingo/n02115641_877.jpg"
+    }
+]
+`
