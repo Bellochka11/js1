5eb065818ab2e62146c200a0956e90fc6a96f419
Bellochka11 <elizavetabelyankina@mail.ru>
2024-10-17 16:05:12 +0300

    g

---
+++ data_11.js
@@ -0,0 +1,34 @@
+const dataProducts = `
+[
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "p": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "img": "product2.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "p": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "img": "product3.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "p": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "img": "product4.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "p": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "img": "product5.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "p": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00",
+        "img": "product6.png"
+    }
+]
+`
